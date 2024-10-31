1e1b1e45992d20fb6a118da153f9386313690b12
matiasFigliuolo <matias.figliuolo1@gmail.com>
2024-10-31 20:16:03 -0300

    Arreglos Generales y de diseño

--- src/app/add-patient/add-patient.component.ts
+++ src/app/add-patient/add-patient.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { AddMedicComponent } from '../add-medic/add-medic.component';
 
 @Component({
   selector: 'app-add-patient',
   templateUrl: './add-patient.component.html',
   styles: ``
 })
+
 export class AddPatientComponent {
 
 }
