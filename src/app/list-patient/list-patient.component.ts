97e61587ae21bfe8b3d6b94253ccbe2830df8677
Luna De Noia <denoialuna@gmail.com>
2024-11-04 19:45:15 -0300

    click + info list patient

--- src/app/list-patient/list-patient.component.ts
+++ src/app/list-patient/list-patient.component.ts
@@ -7,13 +7,18 @@ import { Patient } from '../modules/modules.module';
   templateUrl: './list-patient.component.html',
   styleUrl: './list-patient.component.css'
 })
+
 export class ListPatientComponent implements OnInit {
-patientList: Array<Patient>= [];
+  patientList: Array<Patient>= [];
+  selectedPatient: Patient | null = null;
 
   constructor(private patientService : PatientService){};
   
-  
   ngOnInit(): void {
     this.patientList= this.patientService.getAll();
+  }
+
+  selectPatient(patient: Patient): void {
+    this.selectedPatient = patient;
   }
 }
